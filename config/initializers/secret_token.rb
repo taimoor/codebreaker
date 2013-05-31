e174c618a298dac212ca2b3cbb6959c5e1a7a9aa
Taimoor <taimoor.changaiz@coeus-solutions.de>
2013-05-31 19:40:00 +0500

    code breaker first commit with cucumber

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codebreaker::Application.config.secret_token = 'd1cbd476e6890cc6378de0be5f3a4a33907046bea3fb8f8aa5d367cd676fb2732d148cf8946b8290c37e1e3cfc5b58e82a9bc57149c587f0c5105203dfca50f1'
